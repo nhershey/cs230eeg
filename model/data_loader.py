5a1cdb062ba50214cabdfea9da796d96802cd1ee
nhershey <nhershey@raiders6.stanford.edu>
2018-03-16 15:43:00 -0700

    balanced, padded, standardized

--- model/data_loader.py
+++ model/data_loader.py
@@ -1,4 +1,3 @@
-import random
 import os
 
 
@@ -6,6 +5,8 @@ import pandas as pd
 import numpy as np
 import h5py
 import torch
+
+from tqdm import tqdm
 
 SEIZURE_STRINGS = ['sz','seizure','absence','spasm']
 FREQUENCY = 200
@@ -15,30 +16,20 @@ INCLUDED_CHANNELS = ['EEG Fp1', 'EEG Fp2', 'EEG F3', 'EEG F4', 'EEG C3', 'EEG C4
 'EEG Pg1', 'EEG Pg2', 'EEG A1', 'EEG A2', 'EEG FT9', 'EEG FT10']
 # PATH_TO_DATA = "../../../jdunnmon/EEG/eegdbs/SEC/stanford/"
 PATH_TO_DATA = "../../../jdunnmon/data/EEG/eegdbs/SEC/stanford/"
-# INCLUDED_CHANNELS = ['EEG C3']
+PATH_TO_FILENAMES = "file_markers/"
 
 
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 
+from random import shuffle # used to shuffle initial file names
+from random import randint # used to randomly pull nonSeizures
+
 """ Hershey edits
 Now, we're just loading up the data set by calling get_seizures.getDataLoaders()
 and then returning indices and the length according to the items returned from that
 I left fetch_dataloader as is, so it just creates the same train, test, and dev sets"""
-
-# borrowed from http://pytorch.org/tutorials/advanced/neural_style_tutorial.html
-# and http://pytorch.org/tutorials/beginner/data_loading_tutorial.html
-# define a training image loader that specifies transforms on images. See documentation for more details.
-train_transformer = transforms.Compose([
-    transforms.Resize(64),  # resize the image to 64x64 (remove if images are already 64x64)
-    transforms.RandomHorizontalFlip(),  # randomly flip image horizontally
-    transforms.ToTensor()])  # transform it into a torch tensor
-
-# loader for evaluation, no horizontal flip
-eval_transformer = transforms.Compose([
-    transforms.Resize(64),  # resize the image to 64x64 (remove if images are already 64x64)
-    transforms.ToTensor()])  # transform it into a torch tensor
 
 def getOrderedChannels(labelsObject):
     labels = list(labelsObject)
@@ -46,28 +37,70 @@ def getOrderedChannels(labelsObject):
         labels[i] = labels[i].decode("utf")
     orderedChannels = []
     for ch in INCLUDED_CHANNELS:
-        try:
-            orderedChannels.append(labels.index(ch))
-        except:
-            # print("failed to get channel " + ch, labels)
-            return None
+        orderedChannels.append(labels.index(ch))
     return orderedChannels
 
 def sliceEpoch(orderedChannels, signals, sliceTime):
+    if (sliceTime == -1):
+        maxStart = max(signals.shape[1] - FREQUENCY * EPOCH_LENGTH_SEC, 0)
+        sliceTime = randint(0, maxStart)
+        sliceTime /= FREQUENCY
     startTime = int(FREQUENCY * sliceTime)
     endTime = int(FREQUENCY * (sliceTime + EPOCH_LENGTH_SEC))
     sliceMatrix = signals[orderedChannels,  startTime : endTime]
-    sliceMatrix = sliceMatrix.T
+
     # standardize by row
-    # row_sums = sliceMatrix.sum(axis=1)
-    # sliceMatrix = sliceMatrix / row_sums[:, np.newaxis]
+    row_sums = sliceMatrix.sum(axis=1)
+    row_sums = row_sums[:, np.newaxis]
+    sliceMatrix = sliceMatrix * 1.0 / row_sums
+
+    # pad if necessary
+    diff = FREQUENCY * EPOCH_LENGTH_SEC - sliceMatrix.shape[1]
+    if diff > 0:
+        zeros = np.zeros((sliceMatrix.shape[0], diff))
+        sliceMatrix = np.concatenate((sliceMatrix, zeros))
+
+    sliceMatrix = sliceMatrix.T
     return sliceMatrix
 
+def getSeizureTimes(hdf):
+    annot = hdf['record-0']['edf_annotations']
+    antext = [s.decode('utf-8') for s in annot['texts'][:]]
+    starts100ns = [xx for xx in annot['starts_100ns'][:]]
+    df = pd.DataFrame(data=antext, columns=['text'])
+    df['starts100ns'] = starts100ns
+    df['starts_sec'] = df['starts100ns']/10**7
+    seizureDF = df[df.text.str.contains('|'.join(SEIZURE_STRINGS),case=False)]
+    seizureTimes = seizureDF['starts_sec'].tolist()
+    return seizureTimes
+
+def parseTxtFiles(seizure_file, nonseizure_file):
+    seizure_contents = open(seizure_file, "r")
+    seizure_str = seizure_contents.readlines()
+
+    nonseizure_contents = open(nonseizure_file, "r")
+    nonseizure_str = nonseizure_contents.readlines()
+
+    smaller_half = min(len(seizure_str), len(nonseizure_str))
+
+    combined_str = seizure_str[ : smaller_half] + nonseizure_str[ : smaller_half]
+
+    shuffle(combined_str)
+
+    combined_tuples = []
+    for i in range(len(combined_str)):
+        tup = combined_str[i].strip("\n").split(",")
+        tup[1] = int(tup[1])
+        combined_tuples.append(tup)
+
+    return combined_tuples
 
 class SIGNSDataset(Dataset):
     """
     A standard PyTorch definition of Dataset which defines the functions __len__ and __getitem__.
     """
+    
+
     def __init__(self, data_dir, split_type):
         """
         Store the filenames of the jpgs to use. Specifies transforms to apply on images.
@@ -76,21 +109,23 @@ class SIGNSDataset(Dataset):
             data_dir: (string) directory containing the dataset
             transform: (torchvision.transforms) transformation to apply on image
         """
-        self.filenames = os.listdir(data_dir)
-        total = len(self.filenames)
+        self.file_tuples = parseTxtFiles(PATH_TO_FILENAMES + "seizures_marked.txt", 
+                                            PATH_TO_FILENAMES + "nonSeizures_marked.txt")
+        total = 10 #len(self.file_tuples)
         if split_type == 'train':
-            self.filenames = self.filenames[ : int(total * 0.8)]
+            self.file_tuples = self.file_tuples[ : int(total * 0.8)]
         elif split_type == 'val':
-            self.filenames = self.filenames[int(total * 0.8) : int(total * 0.9)]
+            self.file_tuples = self.file_tuples[int(total * 0.8) : int(total * 0.9)]
         elif split_type == 'test':
-            self.filenames = self.filenames[int(total * 0.9) : int(total)]
+            self.file_tuples = self.file_tuples[int(total * 0.9) : int(total)]
 
         self.data_dir = data_dir
         self.lastSeizure = (torch.zeros(2000, 25), 0)
+        self.num_seiz_shaped = 0
+        self.num_nonseiz_shaped = 0
 
     def __len__(self):
-        # return size of dataset
-        return len(self.filenames)
+        return len(self.file_tuples)
 
     def __getitem__(self, idx):
         """
@@ -103,37 +138,24 @@ class SIGNSDataset(Dataset):
             image: (Tensor) transformed image
             label: (int) corresponding label of image
         """
-        try:
-            currentFileName = self.data_dir + self.filenames[idx]
-            hdf = h5py.File(currentFileName)
-            annot = hdf['record-0']['edf_annotations']
-            antext = [s.decode('utf-8') for s in annot['texts'][:]]
-            starts100ns = [xx for xx in annot['starts_100ns'][:]]
-            df = pd.DataFrame(data=antext, columns=['text'])
-            df['starts100ns'] = starts100ns
-            df['starts_sec'] = df['starts100ns']/10**7
-            seizureDF = df[df.text.str.contains('|'.join(SEIZURE_STRINGS),case=False)]
-            orderedChannels = getOrderedChannels(hdf['record-0']['signal_labels'])
-            if orderedChannels == None:
-                return self.lastSeizure
+        file_name, seizure_idx = self.file_tuples[idx]
 
-            if seizureDF.empty:
-                nonSeizure = sliceEpoch(orderedChannels, hdf['record-0']['signals'], random.randint(0,1.0))
-                nonSeizure = torch.FloatTensor(nonSeizure)
-                if nonSeizure.shape != self.lastSeizure[0].shape:
-                    # print("uh oh")
-                    return self.lastSeizure
-                return (nonSeizure, 0)
-            else:
-                seizureTimes = seizureDF['starts_sec'].tolist()
-                seizure = sliceEpoch(orderedChannels, hdf['record-0']['signals'], seizureTimes[0])
-                seizure = torch.FloatTensor(seizure)
-                if seizure.shape != self.lastSeizure[0].shape:
-                    # print("uh oh")
-                    return self.lastSeizure
-                self.lastSeizure = (seizure, 1)
-                return self.lastSeizure
-        except:
+        currentFileName = self.data_dir + file_name
+        hdf = h5py.File(currentFileName)
+        orderedChannels = getOrderedChannels(hdf['record-0']['signal_labels'])
+
+        if orderedChannels == None:
+            return self.lastSeizure
+
+        if seizure_idx == -1:
+            nonSeizure = sliceEpoch(orderedChannels, hdf['record-0']['signals'], -1)
+            nonSeizure = torch.FloatTensor(nonSeizure)
+            return (nonSeizure, 0)
+        else:
+            seizureTimes = getSeizureTimes(hdf)
+            seizure = sliceEpoch(orderedChannels, hdf['record-0']['signals'], seizureTimes[seizure_idx])
+            seizure = torch.FloatTensor(seizure)
+            self.lastSeizure = (seizure, 1)
             return self.lastSeizure
 
 
@@ -171,9 +193,11 @@ def fetch_dataloader(types, data_dir, params):
 
 if __name__ == '__main__':
     PATH_TO_DATA = "../../../../jdunnmon/data/EEG/eegdbs/SEC/stanford/"
+    PATH_TO_FILENAMES = "../file_markers/"
     sd = SIGNSDataset(PATH_TO_DATA, 'train')
     for i, (train_batch, labels_batch) in enumerate(sd):
-        print(train_batch)
+        print(i, train_batch.shape)
+        # print(sd.num_seiz_shaped, sd.num_nonseiz_shaped)
 
     # sd = SIGNSDataset(PATH_TO_DATA, 'train')
     # print(len(sd))
